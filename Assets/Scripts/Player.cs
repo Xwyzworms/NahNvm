157494a6ebab5077c057f6dd0e1bf6daf0bb38eb
Xwyzworms <pratamaazmi1@gmail.com>
2023-02-09 16:58:25 +0700

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour 
+{
+/**
+    player properties 
+*/
+
+    public int health;
+    public float speed;
+    public string name;
+    public bool isDead;
+
+/**
+    player properties 
+*/
+
+
+        void Start() 
+        {
+
+        }   
+
+        void Update() 
+        {
+
+        }
+}
